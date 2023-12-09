ef38552b1756fca495d3dceff1b16c133e575562
policychanges <policychanges1@gmail.com>
2023-12-09 02:24:59 -0500

    change build

--- build_vlc.sh
+++ build_vlc.sh
@@ -1,5 +1,3 @@
 export CURRENT_VERSION=$(pacman -Si mingw-w64-x86_64-vlc | grep -Po '^Version\s*: \K.+') && \
-wget https://repo.msys2.org/mingw/mingw64/mingw-w64-x86_64-vlc-$(CURRENT_VERSION)-any.pkg.tar.zst && \
-tar -xvf mingw-w64-$(CURRENT_VERSION)-any.pkg.tar.zst && \
-cd mingw-w64-vlc && \
+wget https://github.com/msys2/MINGW-packages/raw/master/mingw-w64-vlc/PKGBUILD && \
 makepkg -s
